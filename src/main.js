85bf5328a480e94666071046886f2ef1958ad4df
Destin Moulton <destin.moulton@gmail.com>
2018-02-19 15:01:54 -0700

    Added store to the init object.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 import Layout from "./components/Layout.vue";
 
+import store from "./store";
 import router from "./routes";
 Vue.use(BootstrapVue);
 
 new Vue({
     el: "#app",
     render: h => h(Layout),
-    router
+    router,
+    store
 });
