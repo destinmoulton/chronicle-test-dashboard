35989c2c31a0590cb4b162d75acc64ebee9594c7
Destin Moulton <destin.moulton@gmail.com>
2018-02-19 14:14:40 -0700

    Added render method for the Layout.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
+import Layout from "./components/Layout.vue";
 
 import router from "./routes";
 Vue.use(BootstrapVue);
 
 new Vue({
     el: "#app",
+    render: h => h(Layout),
     router
 });
