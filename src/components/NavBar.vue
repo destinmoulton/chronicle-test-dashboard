fb0f64f20f3a55fe8389f4452de0caca1546ee70
Destin Moulton <destin.moulton@gmail.com>
2018-02-19 11:31:29 -0700

    Created a navbar component.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+            <b-navbar-nav>
+                <b-nav-item href="#">Link</b-nav-item>
+                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: "navbar"
+};
+</script>
+
