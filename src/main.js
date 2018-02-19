a8660c27de83bd3732be97899998a71c41da9206
Destin Moulton <destin.moulton@gmail.com>
2018-02-19 11:32:56 -0700

    Added bootstrap-vue to the Vue object.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import App from "./App.vue";
+Vue.use(BootstrapVue);
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
