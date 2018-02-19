c11b79c287bdd3e4dd9cbf39ea6ef23beebcb521
Destin Moulton <destin.moulton@gmail.com>
2018-02-19 16:11:32 -0700

    Added localStorage for the server/app settings.

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -1,7 +1,7 @@
 // Initial state
 const state = {
-    server: "",
-    app: ""
+    server: localStorage.getItem("server") || "",
+    app: localStorage.getItem("app") || ""
 };
 
 // getters
@@ -20,6 +20,9 @@ const mutations = {
     setServerAndApp(state, data) {
         state.server = data.server;
         state.app = data.app;
+
+        localStorage.setItem("server", data.server);
+        localStorage.setItem("app", data.app);
     }
 };
 
