bb8025b1dbac81205ac002f550579cc0eccddc87
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 09:54:23 -0700

    Added constants file with the test site data.

---
+++ src/constants/testsite.constants.js
@@ -0,0 +1,9 @@
+export default [
+    { name: "Home", id: "home", content: "Welcome to Acme Inc." },
+    { name: "About", id: "about", content: "About Acme Inc..." },
+    {
+        name: "Contact",
+        id: "contact",
+        content: "Contact Wile Coyote for more information."
+    }
+];
