08ed2e697268d917fc2c23ddcc3aaa4fb492d52c
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 11:35:16 -0700

    Added VueToasted to the main config.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
+import VueToasted from "vue-toasted"; // https://github.com/shakee93/vue-toasted
 import Layout from "./components/Layout.vue";
 
 import store from "./store";
 import router from "./routes";
+
 Vue.use(BootstrapVue);
+Vue.use(VueToasted);
 
 new Vue({
     el: "#app",
