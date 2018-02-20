4834ce3e94d45ab005110d488775e1851d97b7b5
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 11:56:31 -0700

    Created a new component for the Auth actions/buttons.

---
+++ src/components/Dashboard/AuthActions.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <h3>Auth Actions</h3>
+    <b-button v-on:click="login()">Login</b-button>
+    <b-button v-on:click="logout()">Logout</b-button>
+</div>
+</template>
+
+<script>
+import { EventBus } from "../../eventbus";
+export default {
+    methods: {
+        login() {
+            EventBus.$emit("notification", {
+                type: "action",
+                message: "auth:login"
+            });
+        },
+        logout() {
+            EventBus.$emit("notification", {
+                type: "action",
+                message: "auth:logout"
+            });
+        }
+    }
+};
+</script>
+
