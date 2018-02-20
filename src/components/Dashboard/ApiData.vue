1001e8420f16aef04987dfeba80a0fe738993878
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 16:11:36 -0700

    Created an ApiData component.

---
+++ src/components/Dashboard/ApiData.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h4>API Data</h4>
+    <div v-for="swapi in apiData.swapi" v-bind:key="swapi">
+        <strong>{{swapi.name}}</strong>
+        <ul>
+            <li v-for="item in swapi.data" v-bind:key="item.name">{{item.name}}</li>
+        </ul>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            apiData: this.$state.getters.getAPIData
+        };
+    },
+    created() {
+        this.$store.dispatch("downloadAPIData");
+    }
+};
+</script>
