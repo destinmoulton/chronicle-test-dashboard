b8995fa6a248a52da95d0b40115ba2c8b73b018d
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 09:29:22 -0700

    Created a website navigation component.

---
+++ src/components/Dashboard/WebsiteNavigation.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h4>Site Navigation</h4>
+    <b-navbar toggleable="md" type="light" variant="light">
+        <b-navbar-nav>
+            <b-nav-item >Home</b-nav-item>
+            <b-nav-item >About</b-nav-item>
+            <b-nav-item ></b-nav-item>
+        </b-navbar-nav>
+    </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+    name: "website-navigation"
+};
+</script>
+
