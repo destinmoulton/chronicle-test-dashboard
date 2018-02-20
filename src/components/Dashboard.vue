eec74bb80baf34132b8ffb79ca164e7fa8704a6e
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 09:29:41 -0700

    Added the website-navigation component.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,7 +4,22 @@
         <b-col>
             <b-card class="cht-dash-box">
                 <h3>Dashboard</h3>
-                <b-button>Login</b-button>
+            </b-card>
+        </b-col>
+    </b-row>
+    <b-row >
+        <b-col>
+            <b-card class="cht-dash-box">
+                <website-navigation></website-navigation>
+            </b-card>
+        </b-col>
+    </b-row>
+    <b-row >
+        <b-col>
+            <b-card class="cht-dash-box">
+                <h3>Auth Actions</h3>
+                <b-button>Login ("auth:login")</b-button>
+                <b-button>Logout ("auth:logout")</b-button>
             </b-card>
         </b-col>
     </b-row>
@@ -12,8 +27,12 @@
 </template>
 
 <script>
+import WebsiteNavigation from "./Dashboard/WebsiteNavigation";
 export default {
-    name: "dashboard"
+    name: "dashboard",
+    components: {
+        "website-navigation": WebsiteNavigation
+    }
 };
 </script>
 
