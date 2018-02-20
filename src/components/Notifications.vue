ee03383c66f2d067cbc3c723e943ffdc8a5f2582
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 11:34:40 -0700

    Created a Notifications component.

---
+++ src/components/Notifications.vue
@@ -0,0 +1,37 @@
+<template>
+  
+</template>
+
+<script>
+import EventBus from "../eventbus";
+import { NOTIFICATION_CONFIG } from "../constants/notifications.constants.js";
+export default {
+    created() {
+        EventBus.$on(
+            "notification",
+            function(packet = { type: "info", message: "" }) {
+                switch (packet.type) {
+                    case "action":
+                        this.$toasted.info(packet.message, NOTIFICATION_CONFIG);
+                        break;
+                    case "info":
+                        this.$toasted.info(packet.message, NOTIFICATION_CONFIG);
+                        break;
+                    case "error":
+                        this.$toasted.error(
+                            packet.message,
+                            NOTIFICATION_CONFIG
+                        );
+                        break;
+                    case "warn":
+                        this.$toasted.info(packet.message, NOTIFICATION_CONFIG);
+                        break;
+                }
+            }.bind(this)
+        );
+    },
+    destroyed() {
+        //EventBus.$off("notification", this.setMessage);
+    }
+};
+</script>
