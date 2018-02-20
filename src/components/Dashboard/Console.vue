2f8364c7a87f7d2b12b0392e22e902cb8b112273
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 13:44:23 -0700

    Created Console component.

---
+++ src/components/Dashboard/Console.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h4>Console Tests</h4>
+    <b-button v-on:click="info()" variant="primary">Info</b-button>
+    <b-button v-on:click="warn()" variant="warning">Warn</b-button>
+    <b-button v-on:click="error()" variant="danger">Error</b-button>
+</div>
+</template>
+
+<script>
+import { EventBus } from "../../eventbus";
+export default {
+    methods: {
+        error() {
+            EventBus.$emit("notification", {
+                type: "error",
+                message: "ERROR :: Error button pressed."
+            });
+        },
+        info() {
+            EventBus.$emit("notification", {
+                type: "info",
+                message: "INFO :: Info button pressed."
+            });
+        },
+        warn() {
+            EventBus.$emit("notification", {
+                type: "warn",
+                message: "WARN :: Warn button pressed."
+            });
+        }
+    }
+};
+</script>
+
