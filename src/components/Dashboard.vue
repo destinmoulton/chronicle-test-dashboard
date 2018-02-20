f8a1e8b4451b2cc170f3f176b22a94fd7cffc471
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 09:17:54 -0700

    Added margin to the top card.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,14 @@
 <template>
-<b-card>
-    <h3>Dashboard</h3>
-    <b-button>Login</b-button>
-</b-card>
+<b-container>
+    <b-row >
+        <b-col>
+            <b-card class="cht-dash-box">
+                <h3>Dashboard</h3>
+                <b-button>Login</b-button>
+            </b-card>
+        </b-col>
+    </b-row>
+</b-container>
 </template>
 
 <script>
@@ -10,4 +16,10 @@ export default {
     name: "dashboard"
 };
 </script>
+
+<style>
+.cht-dash-box {
+    margin-top: 5px;
+}
+</style>
 
