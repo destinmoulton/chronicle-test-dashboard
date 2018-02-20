1942f8501b65726eb28d3bd1ae11bf449863d3e2
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 11:35:26 -0700

    Added notifications to the Layout.

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,15 +2,18 @@
     <div>
         <navbar></navbar>
         <router-view></router-view>
+        <notifications></notifications>
     </div>
 </template>
 
 <script>
+import Notifications from "./Notifications";
 import NavBar from "./NavBar";
-
+import EventBus from "../eventbus";
 export default {
     components: {
-        navbar: NavBar
+        navbar: NavBar,
+        notifications: Notifications
     }
 };
 </script>
