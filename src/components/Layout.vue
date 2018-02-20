2bb3444b13c1de9282228d0fa8363689e9de7194
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 11:44:23 -0700

    Removed the EventBus from the Layout (moved into the Notifications).

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -9,7 +9,7 @@
 <script>
 import Notifications from "./Notifications";
 import NavBar from "./NavBar";
-import EventBus from "../eventbus";
+
 export default {
     components: {
         navbar: NavBar,
