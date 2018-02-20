d3bfbee40fc41dec601ce99533cd5aa75e724f83
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 09:54:35 -0700

    Added test site generation from the constants.

--- src/components/Dashboard/WebsiteNavigation.vue
+++ src/components/Dashboard/WebsiteNavigation.vue
@@ -1,19 +1,33 @@
 <template>
 <div>
     <h4>Site Navigation</h4>
-    <b-navbar toggleable="md" type="light" variant="light">
+    <b-navbar toggleable="md" type="dark" variant="secondary">
+        <b-navbar-brand >Acme Inc.</b-navbar-brand>
         <b-navbar-nav>
-            <b-nav-item >Home</b-nav-item>
-            <b-nav-item >About</b-nav-item>
-            <b-nav-item ></b-nav-item>
+            <b-nav-item v-for="page in pages" v-bind:key="page.id" v-on:click="changePage(page.id)">
+                {{page.name}}
+            </b-nav-item>
         </b-navbar-nav>
     </b-navbar>
+    <div>{{ content }}</div>
 </div>
 </template>
 
 <script>
+import TEST_SITE from "../../constants/testsite.constants";
 export default {
-    name: "website-navigation"
+    name: "website-navigation",
+    data: () => {
+        return {
+            pages: TEST_SITE,
+            content: TEST_SITE[0].content
+        };
+    },
+    methods: {
+        changePage(newPage) {
+            console.log(newPage);
+        }
+    }
 };
 </script>
 
