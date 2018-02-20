b39c5643fd3e971331f16ce1af823ab49ad29b93
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 16:11:23 -0700

    Added api module to the vuex index.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import api from "./modules/api";
 import settings from "./modules/settings";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
+        api,
         settings
     },
     strict: false
