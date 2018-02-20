c04742407c0a6c8f8a5e764d1bec994f7fb64fd8
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 10:04:18 -0700

    Added Products to the test site information.

--- src/constants/testsite.constants.js
+++ src/constants/testsite.constants.js
@@ -1,6 +1,16 @@
 export default [
     { name: "Home", id: "home", content: "Welcome to Acme Inc." },
-    { name: "About", id: "about", content: "About Acme Inc..." },
+    {
+        name: "About",
+        id: "about",
+        content:
+            "Acme Inc. is the perfect source for all of your cartoonish ideas."
+    },
+    {
+        name: "Products",
+        id: "products",
+        content: "Enough Anvils and TNT to cause mayhem. Same minute shipping!"
+    },
     {
         name: "Contact",
         id: "contact",
