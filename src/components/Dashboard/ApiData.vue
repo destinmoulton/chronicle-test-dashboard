24104be358832ecdeb059481979c822f49079b6d
Destin Moulton <destin.moulton@gmail.com>
2018-02-21 11:46:36 -0700

    Added tabs.

--- src/components/Dashboard/ApiData.vue
+++ src/components/Dashboard/ApiData.vue
@@ -1,12 +1,17 @@
 <template>
 <div>
     <h4>API Data</h4>
-    <div v-for="swapi in apiData.swapi" v-bind:key="swapi.name">
-        <strong>{{swapi.name}}</strong>
-        <ul>
-            <li v-for="item in swapi.data" v-bind:key="item.name">{{item.name}}</li>
-        </ul>
-    </div>
+    <template v-if="apiData.swapi.length > 0">
+    <b-card no-body >
+        <b-tabs card >
+            <b-tab v-for="swapi in apiData.swapi" v-bind:key="swapi.name" v-bind:title="swapi.name" >
+            <ul>
+                <li v-for="item in swapi.data" v-bind:key="item.name">{{item.name}}</li>
+            </ul>
+            </b-tab>
+        </b-tabs>
+    </b-card>
+    </template>
 </div>
 </template>
 
