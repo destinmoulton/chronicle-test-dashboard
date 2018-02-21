4f20ae3aeec921a1297ff021c3d1bbc3229b2f49
Destin Moulton <destin.moulton@gmail.com>
2018-02-21 11:59:41 -0700

    Added textarea with the active data.

--- src/components/Dashboard/ApiData.vue
+++ src/components/Dashboard/ApiData.vue
@@ -4,26 +4,41 @@
     <template v-if="apiData.swapi.length > 0">
     <b-card no-body >
         <b-tabs card >
-            <b-tab v-for="swapi in apiData.swapi" v-bind:key="swapi.name" v-bind:title="swapi.name" >
+            <b-tab v-for="(swapi, sectionIndex) in apiData.swapi" v-bind:key="swapi.name" v-bind:title="swapi.name" >
             <ul>
-                <li v-for="item in swapi.data" v-bind:key="item.name">{{item.name}}</li>
+                <li v-for="(item, itemIndex) in swapi.data" v-bind:key="item.name"><b-button size="sm" v-on:click="activateData('swapi', sectionIndex, itemIndex)">&gt;</b-button>{{item.name}}</li>
             </ul>
             </b-tab>
         </b-tabs>
     </b-card>
     </template>
+    <textarea disabled v-model="activeData"></textarea>
 </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
+    created() {
+        this.$store.dispatch("downloadAPIData");
+    },
+    data: function() {
+        return {
+            activeData: ""
+        };
+    },
     computed: mapGetters({
         apiData: "getAPIData"
     }),
-
-    created() {
-        this.$store.dispatch("downloadAPIData");
+    methods: {
+        activateData(type, sectionIndex, elementIndex) {
+            this.activeData = JSON.stringify(
+                this.$store.getters.getAPIData[type][sectionIndex].data[
+                    elementIndex
+                ]
+            );
+            console.log(this.activeData);
+        }
     }
 };
 </script>
