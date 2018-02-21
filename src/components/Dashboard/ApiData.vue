2fb3244e9425a680ab5a7d786954149fe48c2bd2
Destin Moulton <destin.moulton@gmail.com>
2018-02-20 18:06:51 -0700

    Changed to local function.

--- src/components/Dashboard/ApiData.vue
+++ src/components/Dashboard/ApiData.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <h4>API Data</h4>
-    <div v-for="swapi in apiData.swapi" v-bind:key="swapi">
+    <div v-for="swapi in apiData.swapi" v-bind:key="swapi.name">
         <strong>{{swapi.name}}</strong>
         <ul>
             <li v-for="item in swapi.data" v-bind:key="item.name">{{item.name}}</li>
@@ -12,9 +12,9 @@
 
 <script>
 export default {
-    data: () => {
+    data: function() {
         return {
-            apiData: this.$state.getters.getAPIData
+            apiData: this.$store.getters.getAPIData
         };
     },
     created() {
