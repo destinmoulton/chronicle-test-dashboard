1f2bb75f5e3cf88e8722408a8f4afecb063606a4
Destin Moulton <destin.moulton@gmail.com>
2018-02-22 11:47:41 -0700

    Created ActiveMethods component.

---
+++ src/components/Dashboard/ActiveMethods.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <span v-bind="methods"
+        v-for="method in methods"
+        v-bind:key="method">{{ method }}()</span>
+</div>
+</template>
+
+<script>
+import METHODS from "../../constants/methods.constants.js";
+
+export default {
+    data: function() {
+        return {
+            methods: METHODS,
+            selectedMethods: this.$store.getters.getMethodsToLog
+        };
+    }
+};
+</script>
+
