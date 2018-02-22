4b596e6c29ed5ed36d8abdb5b5eb23e4082795b0
Destin Moulton <destin.moulton@gmail.com>
2018-02-22 11:35:22 -0700

    Added methods array to its own constants file.

---
+++ src/constants/methods.constants.js
@@ -0,0 +1,20 @@
+/**
+ * Array of the possible methods.
+ */
+const METHODS = [
+    "action",
+    "assert",
+    "error",
+    "group",
+    "groupCollapsed",
+    "groupEnd",
+    "info",
+    "log",
+    "table",
+    "time",
+    "timeEnd",
+    "trace",
+    "warn"
+];
+
+export default METHODS;
