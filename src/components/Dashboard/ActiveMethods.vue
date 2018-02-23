a259be2a60a45c46eca930f8b8937f245ba5cee2
Destin Moulton <destin.moulton@gmail.com>
2018-02-22 17:39:37 -0700

    Added mapped getter and an active class.

--- src/components/Dashboard/ActiveMethods.vue
+++ src/components/Dashboard/ActiveMethods.vue
@@ -1,7 +1,9 @@
 <template>
 <div>
+    <h4>console Methods</h4>
     <div
         class="cht-dashboard-activemethods"
+        v-bind:class="{active: (selectedMethods.indexOf(method) > -1)}"
         v-bind:methods="methods"
         v-for="method in methods"
         v-bind:key="method">
@@ -10,9 +12,13 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import METHODS from "../../constants/methods.constants.js";
 
 export default {
+    computed: mapGetters({
+        selectedMethods: "getMethodsToLog"
+    }),
     data: function() {
         return {
             methods: METHODS,
@@ -29,6 +35,11 @@ div.cht-dashboard-activemethods {
     margin: 2px 0px 0px 2px;
     border-radius: 2px;
     border: 1px solid black;
+}
+
+div.cht-dashboard-activemethods.active {
+    color: green;
+    border: 1px solid green;
 }
 
 div.cht-dashboard-activemethods > i {
