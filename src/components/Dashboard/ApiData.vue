034c820f71788b013bf2f8d43255c4f140d9186b
Destin Moulton <destin.moulton@gmail.com>
2018-02-23 09:32:38 -0700

    Added methods to log buttons for the API data.

--- src/components/Dashboard/ApiData.vue
+++ src/components/Dashboard/ApiData.vue
@@ -3,13 +3,14 @@
     <h4>API Data</h4>
     <b-container>
         <b-row>
-            <b-col>
+            <b-col cols="5">
                 <template v-if="apiData.swapi.length > 0">
                 <b-card no-body >
                     <b-tabs pills card vertical>
                         <b-tab v-for="(swapi, sectionIndex) in apiData.swapi" v-bind:key="swapi.name" v-bind:title="swapi.name" >
                         <ul class="cht-list">
-                            <li v-for="(item, itemIndex) in swapi.data" 
+                            <li class="cht-data-link"
+                                v-for="(item, itemIndex) in swapi.data" 
                                 v-bind:key="item.name"
                                 v-on:click="activateData('swapi', sectionIndex, itemIndex)">
                                 {{item.name}}
@@ -20,22 +21,40 @@
                 </b-card>
                 </template>
             </b-col>
-            <b-col>
-                <b-card v-if="activeData.hasOwnProperty('name')">
-                    <b-form-group 
-                        label="Log Message:"
-                        label-for="input-log-message"
-                        >
-                    <b-form-input id="input-log-message"
-                            type="text"
-                            v-model="message"
-                            required
-                            placeholder=""
+            <b-col  cols="5">
+                <b-card >
+                    <template v-if="activeData.hasOwnProperty('name')">
+                        <b-form-group 
+                            label="Log Message:"
+                            label-for="input-log-message"
                             >
-                    </b-form-input>
-                    </b-form-group>
-                    <label>Data to Log</label>
-                    <vue-json-pretty v-bind:data="activeData"></vue-json-pretty>
+                        <b-form-input id="input-log-message"
+                                type="text"
+                                v-model="message"
+                                required
+                                placeholder=""
+                                >
+                        </b-form-input>
+                        </b-form-group>
+                        <label>Data to Log</label>
+                        <vue-json-pretty v-bind:data="activeData"></vue-json-pretty>
+                    </template>
+                </b-card>
+            </b-col>
+            <b-col cols="2">
+                <b-card >
+                    <ul class="cht-list">
+                        <li 
+                            class="cht-method-button"
+                            v-for="method in logMethods" 
+                            v-bind:logMethods="logMethods"
+                            v-bind:key="method"
+                        >
+                    <b-button 
+                        v-bind:disabled="!activeData.hasOwnProperty('name')"
+                        >{{method}}()</b-button>
+                        </li>
+                    </ul>
                 </b-card>
             </b-col>
         </b-row>
@@ -57,11 +76,13 @@ export default {
     data: function() {
         return {
             activeData: "",
-            message: ""
+            message: "",
+            logMethods: this.$store.getters.getMethodsToLog
         };
     },
     computed: mapGetters({
-        apiData: "getAPIData"
+        apiData: "getAPIData",
+        logMethods: "getMethodsToLog"
     }),
     methods: {
         activateData(type, sectionIndex, elementIndex) {
@@ -90,8 +111,12 @@ ul.cht-list {
     padding: 0;
 }
 
-ul.cht-list > li {
+ul.cht-list li.cht-data-link {
     cursor: pointer;
     color: #007bff;
+}
+
+ul.cht-list li.cht-method-button {
+    margin-bottom: 3px;
 }
 </style>
